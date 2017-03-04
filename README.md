b0b2a2b1abc09f023263c49dfe026958257c7e20
liujinmeng <1321839748@qq.com>
2017-03-04 09:18:02 +0800

    my blog

---
+++ README.md
@@ -0,0 +1,13 @@
+# React Transform Boilerplate
+
+## Installation
+
+```bash
+git clone https://github.com/gaearon/react-transform-boilerplate.git
+cd react-transform-boilerplate
+npm install
+npm start
+open http://localhost:3000
+```
+
+Transforms are enabled for files inside `src` (except `index.js`).
---
+++ webpack.config.dev.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.css$/,
+      loader: 'style!css!autoprefixer'
+    },
+    {
+      test: /\.(jpe?g|png)$/,
+      loader: 'file-loader'
+    },
+    {
+      test: /\.(woff|woff2|ttf|svg|eot)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000"
+    }]
+  }
+};
