6e84b776ff75e6253c8a27feca2a62c19eeab98d
liujinmeng <1321839748@qq.com>
2017-03-05 09:57:20 +0800

    post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+import marked from 'marked'
 
 class Post extends React.Component {
   constructor() {
     super();
     this.state={
-
+      data:''
     }
+  }
+  componentDidMount(){
+    axios.get(`https://raw.githubusercontent.com/fightingljm/myblog/master/blogs/${this.props.params.title}`)
+      .then(res => this.setState({data:res.data}))
   }
   render(){
     return(
       <div>
-        {this.props.params.title}
+        {/* {this.props.params.title} */}
+        {
+          this.state.data.length==0 ? '正在加载中' :
+          <div dangerouslySetInnerHTML={{__html:marked(this.state.data)}}></div>
+        }
       </div>
     )
   }
