bc8a2da1c79fcebca59808bc501826d46b9f8551
liujinmeng <1321839748@qq.com>
2017-03-05 22:08:03 +0800

    works

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component {
     return(
       <footer>
         <Link to='/' activeStyle={{color:'#000',fontSize:'20px'}} onlyActiveOnIndex={true}> <i className="fa fa-home" aria-hidden="true"></i><br/> Home </Link>
-        <Link to='/blog' activeStyle={{color:'#5bc0de',fontSize:'20px'}}> <i className="fa fa-bold" aria-hidden="true"></i><br/> Blog </Link>
+        <Link to='/blog' activeStyle={{color:'rgb(0, 188, 212)',fontSize:'20px'}}> <i className="fa fa-bold" aria-hidden="true"></i><br/> Blog </Link>
         <Link to='/work' activeStyle={{color:'#000',fontSize:'20px'}}> <i className="fa fa-desktop" aria-hidden="true"></i><br/> Work </Link>
         <Link to='/about' activeStyle={{color:'#000',fontSize:'20px'}}> <i className="fa fa-font" aria-hidden="true"></i><br/> About </Link>
       </footer>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router'
 
 class Work extends React.Component {
   constructor() {
     super();
     this.state={
-
+      data:[]
     }
+  }
+  componentDidMount(){
+    axios.get('https://raw.githubusercontent.com/fightingljm/myblog/master/blogs/blogs.json')
+      .then(res => this.setState({data:res.data}))
   }
   render(){
     return(
-      <div>
-        work
+      <div style={{width:'100%'}}>
+        {
+          this.state.data.length===0 ? '请稍后' :
+          this.state.data.map(item => (
+            <div key={Math.random()} className='work-card'>
+              {/* <div className='work-img' style={{backgroundImage: 'url(../image/get-started.svg)'}}></div> */}
+              <div className='work-img'></div>
+              <div className='work-title'>
+                {item.title}&nbsp;&nbsp;&nbsp;
+                <span>{item.date}</span>
+              </div>
+            </div>
+          ))
+        }
       </div>
     )
   }
