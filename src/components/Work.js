45f29e9a70de02708f91c89a539e4339766f342c
liujinmeng <1321839748@qq.com>
2017-03-14 12:20:57 +0800

    workStyle and blogs

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import img1 from '../image/get-started.svg'
-import img2 from '../image/css-framework.svg'
+import img1 from '../image/css-framework.svg'
+import img2 from '../image/get-started.jpg'
 import img3 from '../image/components.svg'
+import img4 from '../image/get-started.svg'
 
 class Work extends React.Component {
   constructor() {
@@ -21,24 +22,24 @@ class Work extends React.Component {
             <span>2017.03.03</span>
           </div>
         </div>
-        <div className='work-card'>
+        <div className='work-card wc-p'>
           <div className='work-img' style={{backgroundImage: `url(${img2})`}}></div>
           <div className='work-title'>
-            第一篇文章&nbsp;&nbsp;&nbsp;
+            第二篇文章&nbsp;&nbsp;&nbsp;
             <span>2017.03.03</span>
           </div>
         </div>
         <div className='work-card'>
           <div className='work-img' style={{backgroundImage: `url(${img3})`}}></div>
           <div className='work-title'>
-            第一篇文章&nbsp;&nbsp;&nbsp;
+            第三篇文章&nbsp;&nbsp;&nbsp;
             <span>2017.03.03</span>
           </div>
         </div>
         <div className='work-card'>
-          <div className='work-img' style={{backgroundImage: `url(${img1})`}}></div>
+          <div className='work-img' style={{backgroundImage: `url(${img4})`}}></div>
           <div className='work-title'>
-            第一篇文章&nbsp;&nbsp;&nbsp;
+            第四篇文章&nbsp;&nbsp;&nbsp;
             <span>2017.03.03</span>
           </div>
         </div>
