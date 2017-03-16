4e5cb1031ac1812b36472915f5d7b1b4f6cbafd4
liujinmeng <1321839748@qq.com>
2017-03-16 10:49:41 +0800

    work

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 
-// import img1 from '../image/css-framework.svg'
-// import img2 from '../image/get-started.jpg'
-// import img3 from '../image/components.svg'
-// import img4 from '../image/get-started.svg'
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import CircularProgress from 'material-ui/CircularProgress';
 
@@ -29,34 +24,6 @@ class Work extends React.Component {
     }
     return(
       <div style={{width:'100%'}} className='work-warp'>
-        {/* <div className='work-card'>
-          <div className='work-img' style={{backgroundImage: `url(${img1})`}}></div>
-          <div className='work-title'>
-            第一篇文章&nbsp;&nbsp;&nbsp;
-            <span>2017.03.03</span>
-          </div>
-        </div>
-        <div className='work-card wc-p'>
-          <div className='work-img' style={{backgroundImage: `url(${img2})`}}></div>
-          <div className='work-title'>
-            第二篇文章&nbsp;&nbsp;&nbsp;
-            <span>2017.03.03</span>
-          </div>
-        </div>
-        <div className='work-card'>
-          <div className='work-img' style={{backgroundImage: `url(${img3})`}}></div>
-          <div className='work-title'>
-            第三篇文章&nbsp;&nbsp;&nbsp;
-            <span>2017.03.03</span>
-          </div>
-        </div>
-        <div className='work-card'>
-          <div className='work-img' style={{backgroundImage: `url(${img4})`}}></div>
-          <div className='work-title'>
-            第四篇文章&nbsp;&nbsp;&nbsp;
-            <span>2017.03.03</span>
-          </div>
-        </div> */}
         {this.state.data.length==0 ?
           <div style={styles.circle}>
             <MuiThemeProvider>
@@ -64,16 +31,7 @@ class Work extends React.Component {
             </MuiThemeProvider>
           </div> :
           this.state.data.map( (item,i) => (
-            // <div key={i} className='blog-card'>
-            //   <div className='blog-index'>{i+1}</div>
-            //   <div className='blog-desc'>
-            //     <h3>{item.title}</h3>
-            //     <p>{item.desc}</p>
-            //     <Link to={`post/${item.url}`}>阅读更多</Link>
-            //     <span>{item.date}</span>
-            //   </div>
-            // </div>
-            <div className='work-card'>
+            <div className='work-card' key={Math.random()}>
               <div className='work-img' style={{backgroundImage: `url(${item.img})`}}></div>
               <div className='work-title'>
                 {item.title}&nbsp;&nbsp;&nbsp;
